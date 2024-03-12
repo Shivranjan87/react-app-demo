a45459f96e72b2bd5aac90a2a14005f1fa4a6190
Shubham Awasare <awasareshubham860@gmail.com>
2024-03-12 17:06:43 +0530

    Modified changes are added to repo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a sample Kubernetes demo project V2.0
         </p>
         <a
           className="App-link"
